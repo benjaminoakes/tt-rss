6c1d91edf849057e4e243d82519b7952c265cc3d
Benjamin Oakes <hello@benjaminoakes.com>
2017-08-27 13:58:40 -0500

    Avoid encoding error
        in `encode' on US-ASCII (Encoding::InvalidByteSequenceError)

--- cron_backup.sh
+++ cron_backup.sh
@@ -2,5 +2,5 @@ tmpdir=$(mktemp --directory)
 
 for user_id in $TTRSS_BACKUP_USER_IDS; do
   php tinytinyrss-backup.php $user_id > "$tmpdir/user-$user_id.opml"
-  ruby update_gist.rb "$tmpdir/user-$user_id.opml"
+  LANG=en_US.UTF-8 ruby update_gist.rb "$tmpdir/user-$user_id.opml"
 done
